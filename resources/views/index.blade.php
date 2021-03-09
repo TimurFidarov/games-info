4bafac796958b045c27d72819772b639229f4146
unknown <nowaybuticanfly@gmail.com>
2021-03-09 18:56:44 +0300

    Add and styly recently reviewed section

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,7 +8,7 @@
             <div class="game mt-8">
                 <div class="relative inline-block">
                     <a href="#">
-                        <img  src="/game-poster.jpg" alt="game cover" class="hover:opacity-75=-75 transition ease-in-out duration-150">
+                        <img  src="/game-poster.jpg" alt="game cover" class="hover:opacity-75 transition ease-in-out duration-150">
                     </a>
                     <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full" style="right: -20px;
                     bottom:-20px">
@@ -25,7 +25,7 @@
             <div class="game mt-8">
                 <div class="relative inline-block">
                     <a href="#">
-                        <img  src="/game-poster.jpg" alt="game cover" class="hover:opacity-75=-75 transition ease-in-out duration-150">
+                        <img  src="/game-poster.jpg" alt="game cover" class="hover:opacity-75 transition ease-in-out duration-150">
                     </a>
                     <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full" style="right: -20px;
                     bottom:-20px">
@@ -42,7 +42,7 @@
             <div class="game mt-8">
                 <div class="relative inline-block">
                     <a href="#">
-                        <img  src="/game-poster.jpg" alt="game cover" class="hover:opacity-75=-75 transition ease-in-out duration-150">
+                        <img  src="/game-poster.jpg" alt="game cover" class="hover:opacity-75 transition ease-in-out duration-150">
                     </a>
                     <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full" style="right: -20px;
                     bottom:-20px">
@@ -59,7 +59,7 @@
             <div class="game mt-8">
                 <div class="relative inline-block">
                     <a href="#">
-                        <img  src="/game-poster.jpg" alt="game cover" class="hover:opacity-75=-75 transition ease-in-out duration-150">
+                        <img  src="/game-poster.jpg" alt="game cover" class="hover:opacity-75 transition ease-in-out duration-150">
                     </a>
                     <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full" style="right: -20px;
                     bottom:-20px">
@@ -76,7 +76,7 @@
             <div class="game mt-8">
                 <div class="relative inline-block">
                     <a href="#">
-                        <img  src="/game-poster.jpg" alt="game cover" class="hover:opacity-75=-75 transition ease-in-out duration-150">
+                        <img  src="/game-poster.jpg" alt="game cover" class="hover:opacity-75 transition ease-in-out duration-150">
                     </a>
                     <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full" style="right: -20px;
                     bottom:-20px">
@@ -93,7 +93,7 @@
             <div class="game mt-8">
                 <div class="relative inline-block">
                     <a href="#">
-                        <img  src="/game-poster.jpg" alt="game cover" class="hover:opacity-75=-75 transition ease-in-out duration-150">
+                        <img  src="/game-poster.jpg" alt="game cover" class="hover:opacity-75 transition ease-in-out duration-150">
                     </a>
                     <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full" style="right: -20px;
                     bottom:-20px">
@@ -110,7 +110,7 @@
             <div class="game mt-8">
                 <div class="relative inline-block">
                     <a href="#">
-                        <img  src="/game-poster.jpg" alt="game cover" class="hover:opacity-75=-75 transition ease-in-out duration-150">
+                        <img  src="/game-poster.jpg" alt="game cover" class="hover:opacity-75 transition ease-in-out duration-150">
                     </a>
                     <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full" style="right: -20px;
                     bottom:-20px">
@@ -127,7 +127,7 @@
             <div class="game mt-8">
                 <div class="relative inline-block">
                     <a href="#">
-                        <img  src="/game-poster.jpg" alt="game cover" class="hover:opacity-75=-75 transition ease-in-out duration-150">
+                        <img  src="/game-poster.jpg" alt="game cover" class="hover:opacity-75 transition ease-in-out duration-150">
                     </a>
                     <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full" style="right: -20px;
                     bottom:-20px">
@@ -144,7 +144,7 @@
             <div class="game mt-8">
                 <div class="relative inline-block">
                     <a href="#">
-                        <img  src="/game-poster.jpg" alt="game cover" class="hover:opacity-75=-75 transition ease-in-out duration-150">
+                        <img  src="/game-poster.jpg" alt="game cover" class="hover:opacity-75 transition ease-in-out duration-150">
                     </a>
                     <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full" style="right: -20px;
                     bottom:-20px">
@@ -161,7 +161,7 @@
             <div class="game mt-8">
                 <div class="relative inline-block">
                     <a href="#">
-                        <img  src="/game-poster.jpg" alt="game cover" class="hover:opacity-75=-75 transition ease-in-out duration-150">
+                        <img  src="/game-poster.jpg" alt="game cover" class="hover:opacity-75 transition ease-in-out duration-150">
                     </a>
                     <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full" style="right: -20px;
                     bottom:-20px">
@@ -178,7 +178,7 @@
             <div class="game mt-8">
                 <div class="relative inline-block">
                     <a href="#">
-                        <img  src="/game-poster.jpg" alt="game cover" class="hover:opacity-75=-75 transition ease-in-out duration-150">
+                        <img  src="/game-poster.jpg" alt="game cover" class="hover:opacity-75 transition ease-in-out duration-150">
                     </a>
                     <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full" style="right: -20px;
                     bottom:-20px">
@@ -191,9 +191,173 @@
                     Cyberpunk
                 </a>
                 <div class="text-gray-400 mt-1">PC, Playstation 4</div>
+            </div>
+            <div class="game mt-8">
+                <div class="relative inline-block">
+                    <a href="#">
+                        <img  src="/game-poster.jpg" alt="game cover" class="hover:opacity-75 transition ease-in-out duration-150">
+                    </a>
+                    <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full" style="right: -20px;
+                    bottom:-20px">
+                        <div class="font-semibold text-xs flex justify-center items-center h-full">
+                            80%
+                        </div>
+                    </div>
+                </div>
+                <a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8">
+                    Cyberpunk
+                </a>
+                <div class="text-gray-400 mt-1">PC, Playstation 4</div>
+            </div>
+        </div><!--  end popular games -->
+
+        <div class="flex my-10">
+            <div class="recently-reviewed w-3/4 mr-32">
+                <h2 class="text-blue-500 uppercase tracking-wide font-semibold">Recently Reviewed</h2>
+                <div class="recently-reviewed-container space-y-12 mt-8">
+                    <div class="game bg-gray-800 rounded-lg shadow-md flex px-6 py-6">
+                        <div class="relative flex-none">
+                            <a href="#">
+                                <img  src="/game-poster.jpg" alt="game cover" class="w-48 hover:opacity-75 transition
+                                ease-in-out duration-150">
+                            </a>
+                            <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-900 rounded-full" style="right:
+                            -20px;
+                    bottom:-20px">
+                                <div class="font-semibold text-xs flex justify-center items-center h-full">
+                                    80%
+                                </div>
+                            </div>
+                        </div>
+                        <div class="ml-12">
+                            <a href="#" class="block text-lg font-semibold leading-tight hover:text-gray-400 mt-4">
+                                Cyberpunk
+                            </a>
+                            <div class="text-gray-400 mt-1">
+                                PC, Playstation 5
+                            </div>
+                            <p class="mt-6 text-gray-400">
+                                Cyberpunk 2077 is a role-playing video game developed and published by CD Projekt. Adapted from the Cyberpunk franchise, the game is an open world, non-linear RPG with an FPS style in which players are able to heavily customize their character to suit their play style. Gun play, exploration, player choice and activities such as hacking are to feature heavily throughout the game with missions, quests and objectives being completed in a variety of different ways. The world will have dynamic weather and a day/night cycle to make it truly immersive.
+                            </p>
+                        </div>
+
+                    </div>
+                    <div class="game bg-gray-800 rounded-lg shadow-md flex px-6 py-6">
+                        <div class="relative flex-none">
+                            <a href="#">
+                                <img  src="/game-poster.jpg" alt="game cover" class="w-48 hover:opacity-75 transition
+                                ease-in-out duration-150">
+                            </a>
+                            <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-900 rounded-full" style="right:
+                            -20px;
+                    bottom:-20px">
+                                <div class="font-semibold text-xs flex justify-center items-center h-full">
+                                    80%
+                                </div>
+                            </div>
+                        </div>
+                        <div class="ml-12">
+                            <a href="#" class="block text-lg font-semibold leading-tight hover:text-gray-400 mt-4">
+                                Cyberpunk
+                            </a>
+                            <div class="text-gray-400 mt-1">
+                                PC, Playstation 5
+                            </div>
+                            <p class="mt-6 text-gray-400">
+                                Cyberpunk 2077 is a role-playing video game developed and published by CD Projekt. Adapted from the Cyberpunk franchise, the game is an open world, non-linear RPG with an FPS style in which players are able to heavily customize their character to suit their play style. Gun play, exploration, player choice and activities such as hacking are to feature heavily throughout the game with missions, quests and objectives being completed in a variety of different ways. The world will have dynamic weather and a day/night cycle to make it truly immersive.
+                            </p>
+                        </div>
+
+                    </div>
+                    <div class="game bg-gray-800 rounded-lg shadow-md flex px-6 py-6">
+                        <div class="relative flex-none">
+                            <a href="#">
+                                <img  src="/game-poster.jpg" alt="game cover" class="w-48 hover:opacity-75 transition
+                                ease-in-out duration-150">
+                            </a>
+                            <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-900 rounded-full" style="right:
+                            -20px;
+                    bottom:-20px">
+                                <div class="font-semibold text-xs flex justify-center items-center h-full">
+                                    80%
+                                </div>
+                            </div>
+                        </div>
+                        <div class="ml-12">
+                            <a href="#" class="block text-lg font-semibold leading-tight hover:text-gray-400 mt-4">
+                                Cyberpunk
+                            </a>
+                            <div class="text-gray-400 mt-1">
+                                PC, Playstation 5
+                            </div>
+                            <p class="mt-6 text-gray-400">
+                                Cyberpunk 2077 is a role-playing video game developed and published by CD Projekt. Adapted from the Cyberpunk franchise, the game is an open world, non-linear RPG with an FPS style in which players are able to heavily customize their character to suit their play style. Gun play, exploration, player choice and activities such as hacking are to feature heavily throughout the game with missions, quests and objectives being completed in a variety of different ways. The world will have dynamic weather and a day/night cycle to make it truly immersive.
+                            </p>
+                        </div>
+
+                    </div>
+                    <div class="game bg-gray-800 rounded-lg shadow-md flex px-6 py-6">
+                        <div class="relative flex-none">
+                            <a href="#">
+                                <img  src="/game-poster.jpg" alt="game cover" class="w-48 hover:opacity-75 transition
+                                ease-in-out duration-150">
+                            </a>
+                            <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-900 rounded-full" style="right:
+                            -20px;
+                    bottom:-20px">
+                                <div class="font-semibold text-xs flex justify-center items-center h-full">
+                                    80%
+                                </div>
+                            </div>
+                        </div>
+                        <div class="ml-12">
+                            <a href="#" class="block text-lg font-semibold leading-tight hover:text-gray-400 mt-4">
+                                Cyberpunk
+                            </a>
+                            <div class="text-gray-400 mt-1">
+                                PC, Playstation 5
+                            </div>
+                            <p class="mt-6 text-gray-400">
+                                Cyberpunk 2077 is a role-playing video game developed and published by CD Projekt. Adapted from the Cyberpunk franchise, the game is an open world, non-linear RPG with an FPS style in which players are able to heavily customize their character to suit their play style. Gun play, exploration, player choice and activities such as hacking are to feature heavily throughout the game with missions, quests and objectives being completed in a variety of different ways. The world will have dynamic weather and a day/night cycle to make it truly immersive.
+                            </p>
+                        </div>
+
+                    </div>
+                    <div class="game bg-gray-800 rounded-lg shadow-md flex px-6 py-6">
+                        <div class="relative flex-none">
+                            <a href="#">
+                                <img  src="/game-poster.jpg" alt="game cover" class="w-48 hover:opacity-75 transition
+                                ease-in-out duration-150">
+                            </a>
+                            <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-900 rounded-full" style="right:
+                            -20px;
+                    bottom:-20px">
+                                <div class="font-semibold text-xs flex justify-center items-center h-full">
+                                    80%
+                                </div>
+                            </div>
+                        </div>
+                        <div class="ml-12">
+                            <a href="#" class="block text-lg font-semibold leading-tight hover:text-gray-400 mt-4">
+                                Cyberpunk
+                            </a>
+                            <div class="text-gray-400 mt-1">
+                                PC, Playstation 5
+                            </div>
+                            <p class="mt-6 text-gray-400">
+                                Cyberpunk 2077 is a role-playing video game developed and published by CD Projekt. Adapted from the Cyberpunk franchise, the game is an open world, non-linear RPG with an FPS style in which players are able to heavily customize their character to suit their play style. Gun play, exploration, player choice and activities such as hacking are to feature heavily throughout the game with missions, quests and objectives being completed in a variety of different ways. The world will have dynamic weather and a day/night cycle to make it truly immersive.
+                            </p>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+            <div class="most-anticipated w-1/4">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. A adipisci aliquam cupiditate deleniti dolores, illum iusto nam nihil, nostrum quaerat quasi similique voluptates? Aliquid eaque ex libero omnis ratione, tempore!
             </div>
         </div>
-    </div>
+
+    </div> <!--  end container -->
 
 
 
