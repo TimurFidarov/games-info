2b0f3428626115cfc5a69042545ee9b439788013
unknown <nowaybuticanfly@gmail.com>
2021-03-10 13:24:30 +0300

    add adaptive to header

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,18 +10,18 @@
 </head>
 <body class="bg-gray-900 text-white">
     <header class="border-b border-gray-800">
-        <nav class="container mx-auto flex items-center justify-between px-4 py-6">
-            <div class="flex items-center">
+        <nav class="container mx-auto flex flex-col lg:flex-row items-center justify-between px-4 py-6">
+            <div class="flex items-center flex-col lg:flex-row">
                 <a href="/" style="flex-shrink: 1;">
                     <img src="logo.svg" alt="logo" class="w-32 flex-none">
                 </a>
-                <ul class="flex ml-16 space-x-8">
+                <ul class="flex lg:ml-16 space-x-8 mt-6 lg:mt-0">
                     <li><a href="#" class="hover:text-gray-400">Games</a></li>
                     <li><a href="#" class="hover:text-gray-400">Reviews</a></li>
                     <li><a href="#" class="hover:text-gray-400">Coming soon</a></li>
                 </ul>
             </div>
-            <div class="flex items-center">
+            <div class="flex items-center mt-6 lg:mt-0">
                 <div class="relative">
                     <input type="text" class="bg-gray-800 text-sm rounded-full px-3 py-1 pl-8 w-64 focus:outline-none focus:ring" placeholder="Search...">
                     <div class="absolute top-0 flex items-center h-full ml-2">
