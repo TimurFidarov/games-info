b636e2ed9a3eba773c7448cf9a6be94bc31ab8b3
unknown <nowaybuticanfly@gmail.com>
2021-03-22 10:09:22 +0300

    improve search UX using alpine js

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,6 +8,7 @@
     <link rel="stylesheet" href="/css/app.css">
     <title>Video Games</title>
     <livewire:styles />
+    <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
 </head>
 <body class="bg-gray-900 text-white">
     <header class="border-b border-gray-800">
--- resources/views/livewire/search-dropdown.blade.php
+++ resources/views/livewire/search-dropdown.blade.php
@@ -1,7 +1,22 @@
-<div class="relative">
-    <input wire:model.debounce.300ms="search" type="text" class="bg-gray-800 text-sm rounded-full px-3 py-1 pl-8 w-64
-    focus:outline-none
-    focus:ring" placeholder="Search...">
+<div class="relative" x-data="{ isVisible: true }" @click.away="isVisible = false">
+    <input
+        wire:model.debounce.300ms="search"
+        type="text"
+        class="bg-gray-800 text-sm rounded-full
+        px-3 py-1 pl-8 w-64 focus:outline-none focus:ring"
+        placeholder="Search (Press '/' to focus)"
+        x-ref="search"
+        @keydown.window="
+            if (event.keyCode == 191) {
+                event.preventDefault();
+                $refs.search.focus();
+            }
+        "
+        @keydown = "isVisible = true"
+        @keydown.escape.window="isVisible = false"
+        @focus="isVisible = true"
+        @keydown.shift.tab="isVisible=false"
+    >
     <div class="absolute top-0 flex items-center h-full ml-2">
         <svg class="fill-current text-gray-400 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
             <title>zoom-2</title>
@@ -14,7 +29,7 @@
         <svg class="animate-spin h-4 w-4 rounded-full bg-transparent border-2 border-transparent border-opacity-50" style="border-right-color: white; border-top-color: white;" viewBox="0 0 24 24"></svg>
     </div>
     @if(strlen($search) >=2)
-    <div class="absolute z-10 bg-gray-800 text-xs rounded w-64 mt-2">
+    <div class="absolute z-10 bg-gray-800 text-xs rounded w-64 mt-2" x-show.transition.opacity.duration.200="isVisible">
         @if(count($searchResults) > 0)
         <ul>
             @foreach($searchResults as $game)
@@ -24,7 +39,8 @@
                     ease-in-out
                     duration
                     150 px-3
-                    py-3">
+                    py-3"
+                    @if($loop->last)@keydown.tab="isVisible=false" @endif>
                         @if(isset($game['cover']))
                             <img src="{{Str::replaceFirst('thumb','cover_small', $game['cover']['url'])}}" alt="cover"
                                  class="w-10">
